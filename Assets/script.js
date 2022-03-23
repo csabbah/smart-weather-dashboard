2958074f5c7c69421441d44bc24d768ba81f0218
Csabbah <csabbah@ryerson.ca>
2022-03-23 17:29:56 -0400

    Minor update in JS file for extracting city name after hitting search

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,5 @@
 var APIKEY = '67ad538a4c7356a83bfb4f14c6e9b666';
+
 // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
 // Return Lon and Lat of a given city
@@ -6,24 +7,25 @@ var extractGeoData = async (location) => {
   var url = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${APIKEY}`;
   var res = await fetch(url);
   var location = await res.json();
+
   var lon = location[0].lon;
   var lat = location[0].lat;
-  fetchWeather(lat, lon);
+  fetchWeather(lat, lon, location[0].name);
 };
 
 // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
-var fetchWeather = async (lat, lon) => {
+var fetchWeather = async (lat, lon, location) => {
   var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${APIKEY}&units=imperial`;
   var res = await fetch(url);
   var weatherData = await res.json();
-  extractData(weatherData);
+  extractData(weatherData, location);
 };
 
-var extractData = (weatherData) => {
+var extractData = (weatherData, location) => {
   var feelsLike = weatherData.current.feels_like;
   var currentWeather = weatherData.current.temp;
-  updateEl(currentWeather, feelsLike);
+  updateEl(currentWeather, feelsLike, location);
 };
 
 // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
@@ -32,12 +34,10 @@ var currentWeatherEl = document.getElementById('current-weather');
 var feelslikeEl = document.getElementById('feels-like');
 var citynameEl = document.getElementById('city-name');
 
-var updateEl = (currentWeather, feelsLike) => {
-  var city = inputEl.value;
+var updateEl = (currentWeather, feelsLike, location) => {
   currentWeatherEl.textContent = `${currentWeather}°F`;
   feelslikeEl.textContent = `${feelsLike}°F`;
-  citynameEl.textContent = city;
-  inputEl.value = '';
+  citynameEl.textContent = location;
 };
 // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
@@ -47,4 +47,5 @@ var formEl = document.getElementById('main-form');
 formEl.addEventListener('submit', (e) => {
   e.preventDefault;
   extractGeoData(inputEl.value);
+  inputEl.value = ''; //
 });
